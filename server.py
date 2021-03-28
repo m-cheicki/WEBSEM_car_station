7760e8a46a3de8ea921eb9dec7f092c2ed8cf077
m-cheicki <mcheicki.dev@gmail.com>
2021-03-28 14:10:48 +0200

    👷 404 handling

--- server.py
+++ server.py
@@ -91,5 +91,10 @@ def index():
     return render_template('index.html', all_zipcodes=all_zipcodes, data=data)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(port="5000", debug=True)
